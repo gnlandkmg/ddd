f22aadbab7acb723c1577b6bd81f5226ef357f5e
김경윤 <l982930009@gmail.com>
2026-06-02 10:49:57 +0900

    feat: add first commit

---
+++ src/main/kotlin/jotmang/jotmang/JotmangApplication.kt
@@ -0,0 +1,11 @@
+package jotmang.jotmang
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JotmangApplication
+
+fun main(args: Array<String>) {
+	runApplication<JotmangApplication>(*args)
+}
